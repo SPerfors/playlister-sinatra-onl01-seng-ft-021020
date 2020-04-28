1c2332f320f07aa75c73ad11ab3266aaee1c4803
Steven Perfors <stevecanbuild@gmail.com>
2020-04-28 18:42:33 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,5 +2,5 @@ class Artist < ActiveRecord::Base
   has_many :songs 
   has_many :genres, through: :songs
   #extend Slugifiable::ClassMethods
-  #nclude Slugifiable::InstanceMethods
+  #include Slugifiable::InstanceMethods
 end
