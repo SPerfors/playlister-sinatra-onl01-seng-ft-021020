c64feb97e46bbe67ccbdf2160c7a9962c6c04fb0
Steven Perfors <stevecanbuild@gmail.com>
2020-04-28 18:45:01 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_183011) do
+ActiveRecord::Schema.define(version: 2020_04_28_184420) do
 
   create_table "artists", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "songs", force: :cascade do |t|
     t.string "name"
   end
 
